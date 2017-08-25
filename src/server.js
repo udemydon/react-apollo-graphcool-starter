eb0122728b6a58f74a5f9ef1aa1ba7a51e8d16eb
Gopi <gopi.don@gmail.com>
2017-08-24 19:18:01 -0700

    Notifications Support

--- src/server.js
+++ src/server.js
@@ -16,11 +16,15 @@ import config from '../webpack.config.dev';
 import Environment from './utils/env';
 require('fetch-everywhere');
 
+import Exponent from 'exponent-server-sdk';
+import gcool from './utils/apollo-client';
+import gql from 'graphql-tag';
+
 // initialize the server and configure support for ejs templates
 const app = new Express();
 const server = new Server(app);
 const compiler = webpack(config);
-
+let exponent = new Exponent();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
@@ -35,6 +39,85 @@ app.use(Express.static(path.join(__dirname, 'static')));
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
+function chunkPushNotifications(messages){
+    // Since we can't automatically upgrade everyone using this library, we
+    // should strongly try not to decrease it
+    const chunkLimit = 100;
+    let chunks = [];
+    let chunk = [];
+    for (let message of messages) {
+        chunk.push(message);
+        if (chunk.length >= chunkLimit) {
+            chunks.push(chunk);
+            chunk = [];
+        }
+    }
+    if (chunk.length) {
+        chunks.push(chunk);
+    }
+    return chunks;
+}
+
+async function _sendNotificationAsync(chunk) {
+    try {
+        let receipts = await exponent.sendPushNotificationsAsync(chunk);
+        console.log("Receipts:",receipts);
+    } catch (error) {
+        console.error("Error:",error);
+    }
+}
+
+app.post("/sendNotification", (req, response) => {
+    const id = req.body.id;
+    const title = req.body.title;
+    console.log(id, title)
+    const TokensQuery = gql`
+          query PushTokens {
+            allPushTokens{
+              token
+            }
+          }
+    `;
+
+    gcool.query({
+        query: TokensQuery,
+        fetchPolicy: 'network-only'
+    }).then ((res) => {
+            let tokens =  res.data.allPushTokens;
+            console.log("Tokens:",tokens.length);
+            let allPushNotifs = [];
+            for(var i=0;i<tokens.length; i++){
+                console.log("Sending notif using token:",tokens[i].token);
+                //_sendNotification(id,title, tokens[i].token);
+
+                let isPushToken = Exponent.isExponentPushToken(tokens[i].token);
+                if(isPushToken){
+                    allPushNotifs.push({
+                        to: tokens[i].token,
+                        sound: 'default',
+                        body: title,
+                        data: {id, title, type: 'POST'}
+                    });
+                }
+            }
+            let chunks = chunkPushNotifications(allPushNotifs);
+            //console.log("All Push Notifs:",allPushNotifs);
+            //console.log("Chunks:",chunks);
+            for(let chunk of chunks){
+                            _sendNotificationAsync(chunk);
+            }
+
+
+            response.end("Success sending Notifs");
+
+
+    }, (err) => {
+         console.log("ERROR is:", err);
+         response.end("Error sending Notifs");
+        }
+    )
+
+})
 
 // universal routing and rendering
 app.get('*', (req, res) => {
